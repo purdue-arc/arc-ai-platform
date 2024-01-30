1ccabc8727df1e1d1435fceb9d513b226e9f27dc
pkurto16 <ptkurto3@gmail.com>
2024-01-29 19:19:33 -0500

    Routing Setup

--- arc-ai-platform/src/App.jsx
+++ arc-ai-platform/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./components/landing/Landing.jsx";
+import "./App.css";
 function App() {
-
   return (
-    <>
-      <div>
-          ARC AI Platform
-                         <div>
-
-                </div>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/registration" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ arc-ai-platform/src/components/landing/Landing.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "Landing.css";
+
+function Landing() {
+  return <div></div>;
+}
+
+export default Landing;
