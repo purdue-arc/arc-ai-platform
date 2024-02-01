93903e687fd885fcb8f23fb034d6d4da98a858a7
pkurto16 <ptkurto3@gmail.com>
2024-02-01 11:33:42 -0500

    Better styling (still WIP)

--- src/components/landing/cta-section/CtaSection.jsx
+++ src/components/landing/cta-section/CtaSection.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import "./CtaSection.css";
+import { useNavigate } from "react-router-dom";
 
 function CtaSection() {
+  const navigate = useNavigate();
   return (
     <section className="cta-section">
       <h2>Ready to unlock the power of AI?</h2>
-      <a href="/signup" className="cta-button">
+      <button
+        onClick={() => {
+          navigate("/signup");
+        }}
+        className="cta-button"
+      >
         Sign Up Now
-      </a>
+      </button>
       <p>Start your free trial today and experience the difference.</p>
     </section>
   );
--- src/components/landing/header/Header.jsx
+++ src/components/landing/header/Header.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
     <header className="landing-header">
+      <a href="/" className="header-logo-link">
+        <img
+          src="../../.././assets/react.svg"
+          alt="Logo"
+          className="header-logo"
+        />
+      </a>
       <nav className="landing-nav">
-        <ul>
-          <a href="/" className="header-logo-link">
-            <img
-              src="../../.././assets/react.svg"
-              alt="Logo"
-              className="header-logo"
-            />
-          </a>
-          <li className="nav-link">
-            <a href="/about">ABOUT</a>
-          </li>
-          <li className="nav-link">
-            <a href="/registration">REGISTRATION</a>
-          </li>
-        </ul>
+        <li className="nav-link">
+          <a href="/about">ABOUT</a>
+        </li>
+        <li className="nav-link">
+          <a href="/registration">REGISTRATION</a>
+        </li>
       </nav>
     </header>
   );
