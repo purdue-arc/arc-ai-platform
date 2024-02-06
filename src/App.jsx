99d224adff548ac622b35ad0779e889086f025db
pkurto16 <ptkurto3@gmail.com>
2024-02-05 19:07:46 -0500

    Fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/landing/Landing.jsx";
 import "./App.css";
+import CodeAssistant from "./components/code-assistant/CodeAssistant.jsx";
 function App() {
   return (
     <BrowserRouter>
