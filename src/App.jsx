55bb38b51f42249ae8222f50d722298dda3fa5cb
pkurto16 <ptkurto3@gmail.com>
2024-02-05 19:03:37 -0500

    New code assistant endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/code-assistant" element={<CodeAssistant />} />
       </Routes>
     </BrowserRouter>
   );
