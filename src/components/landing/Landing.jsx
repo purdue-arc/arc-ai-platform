a4b027c7cf62fc40bcb4f66ca5f3d00fcf5f6c99
pkurto16 <ptkurto3@gmail.com>
2024-03-04 14:14:44 -0500

    Parallax

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -5,17 +5,29 @@ import FeaturesSection from "./features-section/FeaturesSection.jsx";
 import CtaSection from "./cta-section/CtaSection.jsx";
 import Footer from "../footer/Footer.jsx";
 import "./Landing.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function Landing() {
   return (
     <div>
-      <Header />
-      <div className="landing-content">
-        <HeroSection />
-        <FeaturesSection />
-        <CtaSection />
-      </div>
-      <Footer />
+      <Parallax pages={2} style={{ top: "0", left: "0" }}>
+        <ParallaxLayer offset={0} speed={1} style={{ zIndex: 1 }}>
+          <Header />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5} style={{ zIndex: -1 }}>
+          <div className="landing-bg"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.15} speed={1} style={{ zIndex: 0 }}>
+          <div className="landing-content">
+            <HeroSection />
+            <FeaturesSection />
+            <CtaSection />
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.5} speed={1} style={{ zIndex: 1 }}>
+          <Footer />
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
