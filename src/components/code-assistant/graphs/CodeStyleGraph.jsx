a5da9137088dfaffc36199d99d42f2b5c2e9c366
ShannonC8 <71951666+ShannonC8@users.noreply.github.com>
2024-03-05 16:50:37 -0500

    Intial working graph version

--- src/components/code-assistant/graphs/CodeStyleGraph.jsx
+++ src/components/code-assistant/graphs/CodeStyleGraph.jsx
@@ -5,76 +5,6 @@ import './CodeStyleGraph.css'; // Import the CSS file
 
 Chart.register(...registerables);
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-    title: {
-      display: true,
-      text: 'Code Style Metrics', // Title for the graph
-      font: {
-        size: 20, // Adjust the font size as needed
-      },
-    },
-    colors: {
-      enabled: true,
-    },
-    tooltip: {
-      callbacks: {
-        title: (tooltipItems) => {
-          return '';
-        },
-        label: function (context) {
-          let label = context.dataset.label || '';
-
-          if (label) {
-            label += 'Participant signup';
-          }
-          if (context.parsed.y !== null) {
-            label += `${context.parsed.y} signups`;
-          }
-          return label;
-        },
-        afterLabel: (tooltipItem) => {
-          return tooltipItem.label;
-        },
-        labelColor() {
-          return {
-            borderColor: 'transparent',
-            backgroundColor: 'transparent',
-            borderWidth: 3,
-          };
-        },
-        labelTextColor: function (context) {
-          return '#050305';
-        },
-        afterLabelTextColor: function (context) {
-          return '#495434';
-        },
-      },
-      backgroundColor: '#fff',
-      borderColor: '#frfrfr',
-      usePointStyle: false,
-      showShadow: true,
-    },
-  },
-  scales: {
-    x: {
-      display: false,
-    },
-    y: {
-      display: false, // Hide the y-axis
-    },
-  },
-  interaction: {
-    intersect: false, // Make bars non-reactive
-    mode: 'index',
-  },
-  events: [], // No events required
-};
-
 const graphData = [10, 50, 40, 90, 10, 20];
 
 const categories = [
@@ -85,13 +15,48 @@ const categories = [
   'Naming',
   'Styling',
 ];
+
 const labels = categories;
 
-export const data = {
+export const options = {
+    aspectRatio: 1,
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Code Style Metrics', // Title for the graph
+        font: {
+          size: 20, // Adjust the font size as needed
+        },
+      },
+      colors: {
+        enabled: true,
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+      },
+      y: {
+        display: false, // Hide the y-axis
+      },
+    },
+    interaction: {
+      intersect: false, // Make bars non-reactive
+      mode: 'index',
+    },
+    events: [], // No events required
+  };
+  
+
+const data = {
   labels,
   datasets: [
     {
-      label: 'Metrics', // Label for the dataset
+      label: labels, // Label for the dataset
       backgroundColor: '#5052ff',
       data: graphData,
       barPercentage: 0.8, // Adjust the width of the bars
@@ -129,7 +94,7 @@ const CodeStyleGraph = () => {
 
   useEffect(() => {
     if (isVisible) {
-      setKey((prevKey) => prevKey + 1); // Remount the Bar component to reanimate
+      setKey(prevKey => prevKey + 1); // Remount the Bar component to reanimate
     }
   }, [isVisible]);
 
